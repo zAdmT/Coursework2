aa5fdb94b08610a149c56690c8409476cd98cf78
zAdmT <adamthomson079@gmail.com>
2024-12-22 12:18:54 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-<server.js content>
+var http = require('http');
+var requests=0;
+var podname= process.env.HOSTNAME;
+var startTime;
+var host;
+
+var handleRequest = function(request, response) {
+  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200);
+  response.write("DevOps Coursework 2! | Running on: ");
+  response.write(host);
+  response.end(" | v=0\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+
+var www = http.createServer(handleRequest);
+www.listen(8080,function () {
+    startTime = new Date();;
+    host = process.env.HOSTNAME;
+    console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+});
+
